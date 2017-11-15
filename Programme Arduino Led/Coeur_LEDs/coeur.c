bd748e5db183ae2afc6759e6bd1386954ec058e6
Jeff Raptor <maxime.jasserand@viacesi.fr>
2017-11-15 14:47:36 +0100

    jeff

--- Programme Arduino Led/Coeur_LEDs/coeur.c
+++ Programme Arduino Led/Coeur_LEDs/coeur.c
@@ -1,39 +1,73 @@
 //#include "coeur.h"
-#include<Arduino.h>
+#include <Arduino.h>
+
 
 void allLEDs(int *tab)
 {
-	//while (A0 == 1)			//tant que le battement est lu
-	//{
+	while (A0 == 1)			//tant que le battement est lu
+	{
 		for (int i = 0; i < 10; i++)
 		{
 			digitalWrite(tab[i] , HIGH);
 		}
 		delay(500);
-	//}
-	//while (A0 == 0)
-	//{
+	}
+	while (A0 == 0)
+	{
 		for (int i = 0; i < 10; i++)
 		{
 			digitalWrite(tab[i] , LOW);
 		}
 		delay(500);
-	//}
-}
-
-void uneLEDsurX(int x)
-{
-	int i = 0;
-	while (A0 == 1)
-	{
-		digitalWrite(i, HIGH);
 	}
 }
 
-void LEDX(int x)
+void uneLEDsurX(int x, int *tab)
 {
+
+	for (int i = 0; A0 == 1 && i < 10; i += x )
+	{
+		digitalWrite(tab[i], HIGH);
+	}
+	delay(500);
+	for (int i = 0; A0 == 0 && i < 10; i += x)
+	{
+		digitalWrite(tab[i], LOW);
+	}
+	delay(500);
 }
 
-void chenille(int x)
+void LEDX(int x, int *tab)
 {
+	while (A0 == 0)
+	{
+		digitalWrite(tab[x], HIGH);
+	}
+	while (A0 == 1)
+	{
+		digitalWrite(tab[x], LOW);
+	}
+}
+
+void chenille(int x, int *tab)
+{
+	for (int i = 0; i < x; i++)
+	{
+		while (A0 != 1);
+		while (A0 == 1)
+		{
+			digitalWrite(tab[i], HIGH);
+			digitalWrite(tab[i + 10 - x], LOW);
+		}
+	}
+
+	for (int i = x; i < 10; i++)
+	{
+		while (A0 != 1);
+		while (A0 == 1)
+		{
+			digitalWrite(tab[i - x], LOW);
+			digitalWrite(tab[i], HIGH);
+		}
+	}
 }
