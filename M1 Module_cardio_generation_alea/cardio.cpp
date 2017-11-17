baef3df27debef9ef8705e8ff80f5fc5c967d299
Snargol <anatole.couasnon@orange.fr>
2017-11-17 11:28:55 +0100

    generation_aleatoire_M1

---
+++ M1 Module_cardio_generation_alea/cardio.cpp
@@ -0,0 +1,72 @@
+#include "cardio.h"
+
+#define calcultime 10000
+#define valeurSeuil 650
+
+int valeurPrecedente;
+int tempsPrecedent;
+int tempsDetection;
+int alternance = 0;
+
+
+
+/*--------------------------------------------------------------------------------------*/
+
+void envoidonnees(int tempsDetection)
+{
+	Serial.print((1000.0 * 60.0) / (tempsDetection - tempsPrecedent), 0);
+	Serial.print(";");
+	Serial.print(tempsDetection - tempsPrecedent);
+}
+
+/*--------------------------------------------------------------------------------------*/
+
+int generateAlea()
+{
+	int limite = random(3000, 5000); //permet de ne pas avoir tout le temps le même pouls
+	if (alternance < limite) // nous somme dans la zone minimale
+	{
+		alternance++;
+		return(300);
+	}
+	else if (alternance >= limite && alternance < 10000) // nous sommes dans la zone maximale
+	{
+		alternance++;
+		return(700);
+	}
+	else // nous revenons à la zone minimale
+	{
+		alternance = 0;
+	}
+}
+
+/*--------------------------------------------------------------------------------------*/
+
+int calculPouls(int valeurActuelle)
+{
+	if (valeurActuelle > valeurSeuil) {  // on est dans la zone max
+		if (valeurPrecedente <= valeurSeuil) {  // est-ce qu'on vient d'y entrer?
+			tempsDetection = millis(); // le temps de Detection est au temps actuel
+			if (tempsDetection > (tempsPrecedent + 300)) {  // ce n'est pas seulement une erreur de précision ?
+				envoidonnees(tempsDetection);
+				tempsPrecedent = tempsDetection; // pour pouvoir acquérir la prochaine valeur
+			}
+		}
+	}
+	valeurPrecedente = valeurActuelle; // pour pouvoir acquérir la prochaine valeur
+}
+
+/*--------------------------------------------------------------------------------------*/
+
+void recupDonnees(int valeurActuelle)
+{
+	int time;
+	time = millis();
+
+	while ((millis() - time)< calcultime)  // la durée de mesure est de X secondes
+	{
+		valeurActuelle = generateAlea(); //on lit la valeur
+		calculPouls(valeurActuelle); // calcul du pouls à partir de la valeur
+	}
+}
+/*--------------------------------------------------------------------------------------*/
---
+++ M1 Module_cardio_generation_alea/cardio.h
@@ -0,0 +1,27 @@
+/*
+Name:		cardio.h
+Created:	14/11/2017
+Author:	Anatole
+*/
+
+#pragma once
+#include <Arduino.h>
+
+//Permet l'envoi de données au port série de l'arduino, qui serons ensuite lues dans le module processing
+//tempsPrecedent : il s'agit du temps d'enregistrement de la valeur précédente
+//tempsDetection : il s'agit du temps d'enregistrement de la valeur actuelle
+void envoidonnees(int tempsPrecedent, int tempsDetection);
+
+
+//Calcul le pouls en fonction des valeurs lues par le capteur
+//valeurActuelle : en paramètre - il s'agit de la valeur lue par le capteur 
+//valeurPrecedente : en référence - il s'agit de la valeur anciennement lue par le capteur
+//tempsPrecedent : en référence - il s'agit du temps d'enregistrement de la valeur précédente
+void calculPouls(int valeurActuelle, int *valeurPrecedente, int *tempsPrecedent);
+
+
+//permet de récuperer les donnees du capteur et d'activer la fonction calculPouls pendant une durée définie (calcultime)
+void recupDonnees(int valeurActuelle);
+
+//Permet de simuler des valeurs de pouls en l'absence des données du capteur
+int generateAlea();
